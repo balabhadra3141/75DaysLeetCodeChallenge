cb4ece1a0339c75a12ca03771888c6b9ac246229
Balabhadra Padhi <padhibalia172@gmail.com>
2026-03-21 22:09:39 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        for(int j = 0; j < nums.size(); j++){
+        int i=0;
+        for(int j=0; j<nums.size(); j++){
             if(nums[j] != 0){
-                nums[i++] = nums[j];
+                swap(nums[j],nums[i]);
+                i++;
             }
-        }
-        while(i < nums.size()){
-            nums[i++] = 0;
         }
     }
 };
