aab1e0cdebc7072795b99fb174cc892898bfb91e
Balabhadra Padhi <padhibalia172@gmail.com>
2026-03-29 21:41:24 +0530

    Time: 0 ms (100%), Space: 9.1 MB (15.14%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> mapping = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+        for (char c : s){
+            if(mapping.find(c) != mapping.end()){
+                if(st.empty() || st.top() != mapping[c]){
+                    return false;
+                }
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
